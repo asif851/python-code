1272cff2b619e2ff10e133aa120e0ca3fffcb3a6
asif851 <asifmredha21@gmail.com>
2023-01-22 23:09:45 +0600

    added onlyme file

---
+++ onlyme.py
@@ -0,0 +1,14 @@
+print("Hey Buddy!!")
+
+auth=input("are you??")
+if auth=='asif':
+    first=input("How is everything going?")
+    quries=input("How may i help you?")
+    if quries =="feeling bored":
+         print("you should listen songs. go to the link: https://www.youtube.com/")
+    if quries =="github":
+    	    print("wanna see your workon github? go to the link: https://github.com/asif851")
+
+
+else:
+    print("see you, bye!!")
