3aefd1625307d749b12603b1a7421bb1009ecae6
asif851 <asifmredha21@gmail.com>
2023-04-29 21:26:12 +0600

    added download-from-link.py

---
+++ convert-mp4-to-mp3/download-from-link.py
@@ -0,0 +1,31 @@
+
+ 
+
+from pytube import YouTube
+import os
+
+path = '/home/asif/Project/python-code/convert-mp4-to-mp3/youtube-download\\'
+
+video_url = input("Please enter the video URL: ")
+yt = YouTube(video_url)
+
+#Get highest bitrate audio stream for given codec (defaults to mp4)
+audio = yt.streams.get_audio_only()
+
+audio.download(output_path=path)
+
+file_name = audio.default_filename
+
+source = path + file_name
+
+if ' ' in file_name:
+    os.rename(source, source.replace(' ', '_'))
+    file_name = source.replace(' ','_')
+
+file_without_ext = os.path.splitext(file_name)[0]
+
+command = f"ffmpeg -i {file_name} {file_without_ext}.mp3"
+
+os.system(command)
+
+os.remove(file_name)
